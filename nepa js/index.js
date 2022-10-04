a1d11a73c3a008ee96114112c3127c4f66d97de3
8sosa <104742680+8sosa@users.noreply.github.com>
2022-10-04 22:26:57 +0100

    Add files via upload

---
+++ nepa js/index.js
@@ -0,0 +1,24 @@
+const text = document.querySelector('.title');
+const changeStatus = document.querySelector('.changeStatus');
+
+changeStatus.addEventListener("click", function(){
+    if(document.getElementById('myImage').src = "img/off.png"){
+        document.getElementById('myImage').src = "img/on.png"
+    }else {
+        document.getElementById('myImage').src = "img/off.png"
+    }
+    
+})
+
+
+
+
+	// function before(){
+	// 	document.getElementById('myImage')
+    //     .src="img/off.png";
+	// }
+	
+	// function afterr(){
+	// 	document.getElementById('myImage')
+	// 	.src="img/on.png";
+	// }
