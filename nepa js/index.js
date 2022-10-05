ae4ae17ead4a66c05b40d4fc638dc97ba5bd47e1
8sosa <104742680+8sosa@users.noreply.github.com>
2022-10-05 17:04:43 +0100

    Add files via upload

--- nepa js/index.js
+++ nepa js/index.js
@@ -1,24 +1,24 @@
-const text = document.querySelector('.title');
-const changeStatus = document.querySelector('.changeStatus');
+// const text = document.querySelector('.title');
+// const changeStatus = document.querySelector('.changeStatus');
 
-changeStatus.addEventListener("click", function(){
-    if(document.getElementById('myImage').src = "img/off.png"){
-        document.getElementById('myImage').src = "img/on.png"
-    }else {
-        document.getElementById('myImage').src = "img/off.png"
-    }
+// changeStatus.addEventListener("click", function(){
+//     if(document.getElementById('myImage').src = "img/off.png"){
+//         document.getElementById('myImage').src = "img/on.png"
+//     }else {
+//         document.getElementById('myImage').src = "img/off.png"
+//     }
     
-})
+// })
 
 
 
 
-	// function before(){
-	// 	document.getElementById('myImage')
-    //     .src="img/off.png";
-	// }
+function before(){
+	document.querySelector('.changeStatus').onclick = afterr
+	document.getElementById('myImage').src="img/off.png";
+}
 	
-	// function afterr(){
-	// 	document.getElementById('myImage')
-	// 	.src="img/on.png";
-	// }
+function afterr(){
+	document.getElementById('myImage').src="img/on.png";
+	document.querySelector('.changeStatus').onclick = before
+}
